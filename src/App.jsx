78fb312a3474ff93cf4c0ec6a8359678a0779cf7
Jason Ericson <jasonlericson@gmail.com>
2026-02-08 15:21:31 -0600

    Added RSTP.ME iframe

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <iframe
+        class="img-rotate"
+        width="640"
+        height="480"
+        src="https://rtsp.me/embed/fbR65NiA/"
+        frameborder="0"
+        title="RTSP Stream Player"
+        allowfullscreen
+      >
+        Iframes not supported. Broadcasting{' '}
+        <a href="https://rtsp.me/" title="rtsp video steaming service">
+          {' '}
+          rtsp.me
+        </a>{' '}
+        player
+      </iframe>
     </>
-  )
+  );
 }
 
 export default App
